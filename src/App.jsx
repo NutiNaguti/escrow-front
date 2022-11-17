932932b986fcc3c9c7cf895cdad0512f2fb098e3
NutiNaguti <derzhavin.danila@icloud.com>
2022-11-17 19:47:50 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,28 @@ import styles from './App.module.css';
 function App() {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+      <header>
+        <div className={styles.Navbar}>
+          <div className={styles.NavbarItem}>
+            Hello
+          </div>
+          <div className={styles.NavbarItem}>
+            Buy
+          </div>
+          <div className={styles.NavbarItem}>
+            Sell
+          </div>
+          <div className={styles.NavbarItem}>
+            Log in
+          </div>
+        </div>
+      </header >
+      <div className={styles.Content}>
+        <div className={styles.Main}>
+          Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.
+        </div>
+      </div>
+    </div >
   );
 }
 
